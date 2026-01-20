e328634909a5f916d6a12230a5b1fb015cc95e90
montrell43 <tmerriweather@persevernow.org>
2026-01-20 12:31:54 -0600

    Newly now commitment

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.noCache())
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], 
+  scriptSrc: ["'self'","trusted-cdn.com"]}}))
 
 
 
