d4982cc6a131cafffc7f96b667d3dcd1740fcce8
montrell43 <tmerriweather@persevernow.org>
2026-01-20 12:02:37 -0600

    Newly now commit

--- myApp.js
+++ myApp.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const helmet = require("helmet");
 
+let ninetyDaysInSeconds = 90*24*60*60;
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+
+
 
 
 
